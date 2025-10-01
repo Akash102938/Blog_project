9084c9d5330614b03502a2f235cc7b4ff3c26a3e
Akash102938 <akashjordan399@gmail.com>
2025-10-01 11:54:32 +0530

    commit changes

--- Blog_App/src/App.jsx
+++ Blog_App/src/App.jsx
@@ -1,15 +1,33 @@
-
-import './App.css'
-
+import { useState,useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+import authService from './appwrite/auth'
+import {login,logout} from "./store/authSlice"
+import {Header,Footer} from './components'
 function App() {
+  const [loading,setLoading] = useState(true)
+  const dispatch = useDispatch()
   
-  
+  useEffect(()=>{
+    authService.getCurrentUser()
+    .then((userData)=>{
+      if(userData){
+        dispatch(login({userData}))
+      }
+      else{
+        dispatch(logout())
+      }
+    })
+    .finally(()=> setLoading(false))
+  },[])
 
-  return (
-    <>
-    <h1>blog</h1>
-    </>
-  ) 
+  return !loading ?(
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header/>
+        <Footer/>
+      </div>
+    </div>
+  ):null
 }
 
 export default App
